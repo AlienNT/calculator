9877f1f34aa3ae07f6bf37b9a6ecc6d40439fe7c
Никита Тристень <nick.tristen22@gmail.com>
2022-07-02 20:19:21 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ git add -A
 git commit -m "new build $(date '+%d-%m-%y %H:%M:%S')"
 
 git push origin master
-git subtree push --prefix dist origin gh-pages
-git subtree pull --prefix dist origin gh-pages
+git subtree push --prefix dist origin prod
+git subtree pull --prefix dist origin prod
 
 
