a608c9d3aa8bd631dbf455b1919ccf317c0f2c71
Никита Тристень <nick.tristen22@gmail.com>
2022-07-02 22:00:11 +0300

    merge dev 02-07-22 22:00:11

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,23 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # остановить публикацию при ошибках
 
 # сборка
-git checkout master
-
-echo "--------------------------------------------"
 git merge dev
-
-echo "--------------------------------------------"
 git status
-
-echo "--------------------------------------------"
-npm run build
-
-echo "--------------------------------------------"
 git add -A
-
-$ echo "--------------------------------------------"
-git commit -m "new build $(date '+%d-%m-%y %H:%M:%S')"
-
-echo "--------------------------------------------"
+git commit -m "merge dev $(date '+%d-%m-%y %H:%M:%S')"
 git push origin master
 
-echo "--------------------------------------------"
-git subtree push --prefix dist origin prod
 
-echo "--------------------------------------------"
-git checkout dev
+npm run build
+git add -A
+
+git commit -m "new build $(date '+%d-%m-%y %H:%M:%S')"
+git push origin master
+
+#git subtree push --prefix dist origin prod
+#git checkout dev
 
 $SHELL
 
