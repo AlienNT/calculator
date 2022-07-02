f14175058498c266c8576faf67aee54bbbccc580
Никита Тристень <nick.tristen22@gmail.com>
2022-07-02 20:47:25 +0300

    test build

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,11 @@ git merge dev
 
 npm run build
 
-git init
 git add -A
 git commit -m "new build $(date '+%d-%m-%y %H:%M:%S')"
 
 git push origin master
 git subtree push --prefix dist origin prod
-git subtree pull --prefix dist origin prod
 
 git checkout dev
 
