b5ff295c4e45d3eec2c8d79983c64605753b17a5
Никита Тристень <nick.tristen22@gmail.com>
2022-07-02 20:41:22 +0300

    изменил скрипт деплоя

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@
 set -e
 
 # сборка
+git checkout master
+git merge dev
+
 npm run build
 
 git init
@@ -13,5 +16,7 @@ git commit -m "new build $(date '+%d-%m-%y %H:%M:%S')"
 git push origin master
 git subtree push --prefix dist origin prod
 git subtree pull --prefix dist origin prod
+
+git checkout dev
 
 
