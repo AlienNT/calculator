e4cc356e1ac4ce9053ca22e552db70033dcccf4c
Никита Тристень <nick.tristen22@gmail.com>
2022-07-02 22:12:06 +0300

    merge dev 02-07-22 22:12:06

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m "new build $(date '+%d-%m-%y %H:%M:%S')"
 git push origin master
 
-git subtree push --prefix dist origin -f prod
+git subtree rebase --prefix dist origin prod
 #git checkout dev
 
 $SHELL
