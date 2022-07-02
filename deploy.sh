db96a59bc75f128d2697abe3f891ffb621278a8a
Никита Тристень <nick.tristen22@gmail.com>
2022-07-02 20:01:32 +0300

    new build 02-07-22 20:01:32

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-##!/usr/bin/env sh
+#!/usr/bin/env sh
 
 # остановить публикацию при ошибках
 set -e
@@ -6,28 +6,12 @@ set -e
 # сборка
 npm run build
 
-# переход в каталог сборки
-#cd dist
-
-# если вы публикуете на пользовательский домен
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m "new build $(date '+%d-%m-%y %H:%M:%S')"
 
-git status
-
-# если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-#git push origin master
+git push origin master
 git subtree push --prefix dist origin gh-pages
-
-#git push -f git@github.com:AlienNT/calculator.git master
-#git subtree push --prefix dist git@github.com:AlienNT/calculator.git master:gh-pages
+git subtree pull --prefix dist origin gh-pages
 
 
-#cd -
