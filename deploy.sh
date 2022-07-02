54120814cea60a92c570d4881c24aa83a940b641
Никита Тристень <nick.tristen22@gmail.com>
2022-07-02 21:16:23 +0300

    правки конфига

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,29 @@
 
 # сборка
 git checkout master
+
+echo "--------------------------------------------"
 git merge dev
 
+echo "--------------------------------------------"
+git status
+
+echo "--------------------------------------------"
 npm run build
 
+echo "--------------------------------------------"
 git add -A
+
+$ echo "--------------------------------------------"
 git commit -m "new build $(date '+%d-%m-%y %H:%M:%S')"
 
+echo "--------------------------------------------"
 git push origin master
+
+echo "--------------------------------------------"
 git subtree push --prefix dist origin prod
 
+echo "--------------------------------------------"
 git checkout dev
 
 $SHELL
